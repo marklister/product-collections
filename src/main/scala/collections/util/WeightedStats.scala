3c283237b74a9528a8a30742132c7de70381b4f6
Mark Lister <toomuchspam@nowhere>
2013-06-02 10:10:38 +0300

    scaladoc tuning

--- src/main/scala/collections/util/WeightedStats.scala
+++ src/main/scala/collections/util/WeightedStats.scala
@@ -26,22 +26,23 @@ import math.pow
  */
 
 class WeightedStats[A,B](underlying:Iterable[Product2[A,B]])(
-    implicit private val numA:Numeric[A], implicit private val numB:Numeric[B]){
+  implicit private val numA:Numeric[A], implicit private val numB:Numeric[B]){
   if(underlying.isEmpty) throw new Exception("Empty Seq")
   
+  private lazy val u = underlying
   /**
    * The weighted arithmetic mean of all the elements
    */ 
   lazy val mean :Double= 
-    underlying.map(x=>(numA.toDouble(x._1), numB.toDouble(x._2)))
-  .map(x=>x._1*x._2).sum / numB.toDouble(underlying.map(_._2).sum)
+    u.map(x=>(numA.toDouble(x._1), numB.toDouble(x._2)))
+  .map(x=>x._1*x._2).sum / numB.toDouble(u.map(_._2).sum)
                       
   /**
    * The weighted variance of all the elements (the one divided by n not n-1)
    */
   lazy val variance :Double = 
-    underlying.map (x=>pow(numA.toDouble(x._1)-mean,2)*numB.toDouble(x._2)).sum /
-      numB.toDouble(underlying.map(_._2).sum)
+    u.map (x=>pow(numA.toDouble(x._1)-mean,2)*numB.toDouble(x._2)).sum /
+  numB.toDouble(u.map(_._2).sum)
   
   /**
    * The weighted standard deviation of all the elements
