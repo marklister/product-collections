f7d836bd5edeb866e104a875b613d0800ed3d5ef
Mark Lister <toomuchspam@nowhere>
2013-06-02 10:09:52 +0300

    tidy up root

--- doc2.sh
+++
@@ -1,4 +0,0 @@
-mkdir target/scala-2.10/api
-cd target/scala-2.10/api
-/home/mark/scalaProjects/scala/dists/latest/bin/scaladoc -feature ../../../src/main/scala/collections/*.scala ../../../src/main/scala/collections/io/*.scala  ../../../src/main/scala/collections/immutable/*.scala ../../../src/main/scala/collections/util/*.scala  ../../../target/scala-2.10/src_managed/main/collections/io/*.scala ../../../target/scala-2.10/src_managed/main/collections/immutable/*.scala
-cd ../../..
