982f4e6111e9f57a9bd8a1cb4fe0943c9ef964e9
Mark Lister <toomuchspam@nowhere>
2013-12-25 14:15:15 +0100

    build failed sbt-cross-build-version

--- project/project/build.scala
+++ project/project/build.scala
@@ -2,5 +2,5 @@ import sbt._
 object PluginDef extends Build {
 	override lazy val projects = Seq(root)
 	lazy val root = Project("plugins", file(".")) dependsOn( webPlugin )
-	lazy val webPlugin = uri("git://github.com/marklister/sbt-boilerplate.git#v0.5.2")
+	lazy val webPlugin = uri("git://github.com/marklister/sbt-boilerplate.git#v0.5.2.1")
 }
