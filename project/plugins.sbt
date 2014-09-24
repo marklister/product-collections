3930378fc9d4ed848d3a0c0ebc9a48499a20ca7e
Mark Lister <toomuchspam@nowhere>
2014-09-24 13:46:01 +0200

    switch to sbt-boilerplate 0.5.9 binary

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-//addSbtPlugin("io.spray" % "sbt-boilerplate" % "0.5.1.1") //alternatively we use the source in git see project/project/build.scala
+addSbtPlugin("io.spray" % "sbt-boilerplate" % "0.5.9") //alternatively we use the source in git see project/project/build.scala
 
 //addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.6.2")
 
--- project/project/build.scala
+++ project/project/build.scala
@@ -1,6 +1,6 @@
-import sbt._
-object PluginDef extends Build {
-	override lazy val projects = Seq(root)
-	lazy val root = Project("plugins", file(".")) dependsOn( webPlugin )
-	lazy val webPlugin = uri("git://github.com/marklister/sbt-boilerplate.git#v0.5.2.2")
-}
+//import sbt._
+//object PluginDef extends Build {
+//	override lazy val projects = Seq(root)
+//	lazy val root = Project("plugins", file(".")) dependsOn( webPlugin )
+//	lazy val webPlugin = uri("git://github.com/marklister/sbt-boilerplate.git#v0.5.2.2")
+//}
