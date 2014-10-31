04de78fb5fb58e2baa1ab948f5a7642fda2df5d7
Mark Lister <toomuchspam@nowhere>
2014-10-31 12:47:50 +0200

    tidy up CsvOutputUtils

--- src/main/scala/collections/io/CsvOutputUtils.scala
+++ src/main/scala/collections/io/CsvOutputUtils.scala
@@ -7,14 +7,13 @@
 package com.github.marklister
 package collections.io
 
-import java.io.PrintWriter
-
 import com.github.marklister.collections.immutable.CollSeq
 
 object CsvOutputUtils {
 
   /**
-   * Adds method csvIterator:Iterator[String] to a CollSeq
+   * Adds method csvIterator:Iterator[String] and
+   * writeCsv(w:java.io.Writer) to a CollSeq
    */
   implicit class CsvOutput(c: CollSeq) {
 
@@ -32,8 +31,8 @@ object CsvOutputUtils {
       c.iterator.map(productToCsv(_))
     }
 
-    def writeCsv(w: java.io.Writer) = {
-      csvIterator().map(_ + "\r\n")
+    def writeCsv(w: java.io.Writer,separator:String=","):Unit ={
+      csvIterator(separator).map(_ + "\r\n")
         .foreach(w.write(_))
     }
   }
