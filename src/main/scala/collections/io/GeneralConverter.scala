c2870bea267ad3169fae74fd57036844bbca0eec
Mark Lister <toomuchspam@nowhere>
2015-01-20 18:43:16 +0200

    Make converters more compact.  Implement apply; change to vals not objects.

--- src/main/scala/collections/io/GeneralConverter.scala
+++ src/main/scala/collections/io/GeneralConverter.scala
@@ -2,7 +2,7 @@
  * product-collections is distributed under the 2-Clause BSD license. See the
  * LICENSE file in the root of the repository.
  *
- * Copyright (c) 2013 - 2014 Mark Lister
+ * Copyright (c) 2013 - 2015 Mark Lister
  */
 
 
@@ -34,37 +34,12 @@ import scala.annotation.implicitNotFound
  * Note: the above example is a bit contrived because a pre-existing
  * DateConverter class exists.  See the [[com.github.marklister.collections.io.DateConverter]] docs.
  */
-@implicitNotFound("""No converter found for String => ${A}
-          You can define your own converter:
+@implicitNotFound( """No converter found for String => ${A}
+          You can define your own converters:
           see https://github.com/marklister/product-collections#examples""")
-abstract class GeneralConverter[A]() {
-  /**
-   * Convert (String) => A
-   */
-  val convert: String => A
-
-  /**
-   * Convert (String) => Option[A]
-   */
-  val toOption: (String) => Option[A] = x => Try(convert(x)).toOption
-}
-
-/**
- * A converter for dates (intended to be used by CsvParser) using an underlying 
- * java.text.SimpleDateFormat.
- *
- * You can create one like so:
- * {{{
- * implicit val ymd=new DateConverter("yyyy-MM-dd")
- * }}}
- *
- * The compiler will then provide this converter to CsvParser whenever
- * you try to parse a java.util.Date
- */
-class DateConverter(pattern: String) extends GeneralConverter[java.util.Date] {
-  private val sdf = new java.text.SimpleDateFormat(pattern)
-
-  val convert = (x: String) => sdf.parse(x.trim)
+class GeneralConverter[A](val converter: String => A) {
+  def apply(s: String) = converter(s)
+  def toOption = new GeneralConverter(x => Try(converter(x)).toOption)
 }
 
 /**
@@ -73,90 +48,32 @@ class DateConverter(pattern: String) extends GeneralConverter[java.util.Date] {
  */
 object GeneralConverter {
 
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to a String
-   * Although the conversion is pointless we need an implicit conversion in scope that does this.
-   */
-  implicit object StringConverter extends GeneralConverter[String] {
-    val convert = (x: String) => identity(x)
-  }
+  implicit val StringConverter: GeneralConverter[String] = new GeneralConverter(identity(_))
+  implicit val IntConverter: GeneralConverter[Int] = new GeneralConverter(_.trim.toInt)
+  implicit val LongConverter: GeneralConverter[Long] = new GeneralConverter(_.trim.toLong)
+  implicit val ByteConverter: GeneralConverter[Byte] = new GeneralConverter(_.trim.toByte)
+  implicit val ShortConverter: GeneralConverter[Short] = new GeneralConverter(_.trim.toShort)
+  implicit val FloatConverter: GeneralConverter[Float] = new GeneralConverter(_.trim.toFloat)
+  implicit val DoubleConverter: GeneralConverter[Double] = new GeneralConverter(_.trim.toDouble)
+  implicit val BooleanConverter: GeneralConverter[Boolean] = new GeneralConverter(_.trim.toBoolean)
+  implicit val OptionIntConverter: GeneralConverter[Option[Int]] = IntConverter.toOption
+  implicit val OptionDoubleConverter = DoubleConverter.toOption
+  implicit val OptionLongConverter = LongConverter.toOption
+  implicit val OptionBooleanConverter = BooleanConverter.toOption
 
   /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to an Int
+   * A converter for dates (intended to be used by CsvParser) using an underlying
+   * java.text.SimpleDateFormat.
+   *
+   * You can create one like so:
+   * {{{
+   * implicit val ymd=new DateConverter("yyyy-MM-dd")
+   * }}}
+   *
+   * The compiler will then provide this converter to CsvParser whenever
+   * you try to parse a java.util.Date
    */
-  implicit object IntConverter extends GeneralConverter[Int] {
-    val convert = (x: String) => x.trim.toInt
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to a Long
-   */
-  implicit object LongConverter extends GeneralConverter[Long] {
-    val convert = (x: String) => x.trim.toLong
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to a Byte
-   */
-  implicit object ByteConverter extends GeneralConverter[Byte] {
-    val convert = (x: String) => x.trim.toByte
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to a Short
-   */
-  implicit object ShortConverter extends GeneralConverter[Short] {
-    val convert = (x: String) => x.trim.toShort
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to a Float
-   */
-  implicit object FloatConverter extends GeneralConverter[Float] {
-    val convert = (x: String) => x.trim.toFloat
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to a Double
-   */
-  implicit object DoubleConverter extends GeneralConverter[Double] {
-    val convert = (x: String) => x.trim.toDouble
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to a Boolean
-   */
-  implicit object BooleanConverter extends GeneralConverter[Boolean] {
-    val convert = (x: String) => x.trim.toBoolean
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to an Option[Int]
-   */
-  implicit object OptionIntConverter extends GeneralConverter[Option[Int]] {
-    val convert = IntConverter.toOption
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to an Option[Double]
-   */
-  implicit object OptionDoubleConverter extends GeneralConverter[Option[Double]] {
-    val convert = DoubleConverter.toOption
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to an Option[Long]
-   */
-  implicit object OptionLongConverter extends GeneralConverter[Option[Long]] {
-    val convert = LongConverter.toOption
-  }
-
-  /**
-   * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to an Option[Boolean]
-   */
-  implicit object OptionBooleanConverter extends GeneralConverter[Option[Boolean]] {
-    val convert = BooleanConverter.toOption
-  }
+  class DateConverter(pattern: String) extends GeneralConverter[java.util.Date](new java.text.SimpleDateFormat(pattern).parse)
 
   /**
    * A [[com.github.marklister.collections.io.GeneralConverter]] that converts a String to a Date
--- src/test/scala/ConverterTest.scala
+++ src/test/scala/ConverterTest.scala
@@ -1,9 +1,8 @@
 /*
- * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
+ * product-collections is distributed under the 2-Clause BSD license. See the
+ * LICENSE file in the root of the repository.
+ *
+ * Copyright (c) 2013 - 2015 Mark Lister
  */
 
 import utest._
@@ -13,7 +12,7 @@ import com.github.marklister.collections._
 
 object ConverterTest extends TestSuite {
 
-  def convert[T](s:String)(implicit c:GeneralConverter[T])=c.convert(s)
+  def convert[T](s:String)(implicit c:GeneralConverter[T])=c(s)
 
   val tests = TestSuite{
    'Int {
