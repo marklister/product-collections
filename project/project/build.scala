64aebaab5626eb8cbdf85e79670ba2ccfd2b4c1c
Mark Lister <toomuchspam@nowhere>
2015-02-28 17:48:19 +0200

    Use lastest sbt-boilerplate

--- project/project/build.scala
+++ project/project/build.scala
@@ -3,5 +3,5 @@ import sbt._
 object PluginDef extends Build {
   override def projects = Seq(root)
   lazy val root = Project("plugins", file(".")) dependsOn(boilerplate)
-  lazy val boilerplate = uri("https://github.com/marklister/sbt-boilerplate.git#0.5.9.1-unmanaged")
+  lazy val boilerplate = uri("https://github.com/marklister/sbt-boilerplate.git#013afd0becf8171c05206e227b2dbe72d98c6344")
 }
