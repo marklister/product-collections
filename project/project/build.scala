1de274c556c1e781ba14c0c35a5e2345d3b9b26c
Johannes Rudolph <johannes.rudolph@gmail.com>
2016-03-01 17:01:31 +0100

    use sbt-boilerplate 0.6.0

--- project/project/build.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-
-object PluginDef extends Build {
-  override def projects = Seq(root)
-  lazy val root = Project("plugins", file(".")) dependsOn(boilerplate)
-  lazy val boilerplate = uri("https://github.com/marklister/sbt-boilerplate.git#013afd0becf8171c05206e227b2dbe72d98c6344")
-}
